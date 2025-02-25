2ab65ecd3c87e9a3046d8ff04c3ddc6f05c3781e
jdkwer <hendervergara@gmail.com>
2025-02-25 18:28:14 +0800

    changes the design

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -4,51 +4,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel 11 Crud</title>
+    <title>Laravel 11 CRUD</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    {{-- here we will pass list of all users --}}
-    <div class="container">
-        <div class="card">
-            <div class="card-header">
-                Student Registration
-                <a href="/add/user" class="btn btn-success btn-sm float-end">Add New</a>
+    <div class="container mt-5">
+        <div class="card shadow-lg rounded">
+            <div class="card-header bg-primary text-white d-flex justify-content-between align-items-center">
+                <h5 class="mb-0">Student Registration</h5>
+                <a href="/add/user" class="btn btn-success btn-sm">Add New</a>
             </div>
-            @if (Session::has('success'))
-                <span class="alert alert-success p-2">{{Session::get('success')}}</span>
-            @endif
-            @if (Session::has('fail'))
-                <span class="alert alert-danger p-2">{{Session::get('fail')}}</span>
-            @endif
             <div class="card-body">
-                <table class="table table-sm table-striped table-bordered">
-                    <thead>
-                        <th>S/N</th>
-                        <th>Full name</th>
-                        <th>Email</th>
-                        <th>Phone Number</th>
-                        <th>Registration Date</th>
-                        <th>Last Update</th>
-                        <th colspan="2">Action</th>
+                @if (Session::has('success'))
+                    <div class="alert alert-success">{{ Session::get('success') }}</div>
+                @endif
+                @if (Session::has('fail'))
+                    <div class="alert alert-danger">{{ Session::get('fail') }}</div>
+                @endif
+                <table class="table table-hover table-bordered text-center">
+                    <thead class="table-dark">
+                        <tr>
+                            <th>#</th>
+                            <th>Full Name</th>
+                            <th>Email</th>
+                            <th>Phone Number</th>
+                            <th>Registration Date</th>
+                            <th>Last Update</th>
+                            <th>Actions</th>
+                        </tr>
                     </thead>
                     <tbody>
                         @if (count($all_users) > 0)
                             @foreach ($all_users as $item)
                                 <tr>
-                                    <td>{{$loop->iteration}}</td>
-                                    <td>{{$item->name}}</td>
-                                    <td>{{$item->email}}</td>
-                                    <td>{{$item->phone_number}}</td>
-                                    <td>{{$item->created_at}}</td>
-                                    <td>{{$item->updated_at}}</td>
-                                    <td><a href="/edit/{{$item->id}}" class="btn btn-primary btn-sm">Edit</a></td>
-                                    <td><a href="/delete/{{$item->id}}" class="btn btn-danger btn-sm">Delete</a></td>
-                                </tr>    
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $item->name }}</td>
+                                    <td>{{ $item->email }}</td>
+                                    <td>{{ $item->phone_number }}</td>
+                                    <td>{{ $item->created_at->format('d M Y') }}</td>
+                                    <td>{{ $item->updated_at->format('d M Y') }}</td>
+                                    <td>
+                                        <a href="/edit/{{ $item->id }}" class="btn btn-sm btn-warning">Edit</a>
+                                        <a href="/delete/{{ $item->id }}" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')">Delete</a>
+                                    </td>
+                                </tr>
                             @endforeach
                         @else
                             <tr>
-                                <td colspan="8">No User Found!</td>
+                                <td colspan="7" class="text-center">No User Found!</td>
                             </tr>
                         @endif
                     </tbody>
